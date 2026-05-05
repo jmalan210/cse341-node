c5b4cf679e0e1802418ef33d525cc34cb71121a9
jmalan210 <>
2026-05-04 19:39:43 -0600

    Remove node_modules and add gitiginore

---
+++ connection.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+
+
+async function main() {
+
+    const uri = process.env.MONGO_DB_STRING;
+
+    const client = new MongoClient(uri);
+try {
+    await client.connect();
+    await listDatabases(client);
+    }
+catch (e) {
+    console.error(e);
+    }
+finally {
+    await client.close();
+}
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(`- ${db.name}`));
+    
+}
